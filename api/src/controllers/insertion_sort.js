e63c959b30e435799b87afe289300ff206890327
mariachi9999 <57735911+mariachi9999@users.noreply.github.com>
2021-11-27 10:10:05 -0300

    route /insertion working ok

--- api/src/controllers/insertion_sort.js
+++ api/src/controllers/insertion_sort.js
@@ -1,9 +1,37 @@
 async function insertion_sort(req, res, next) {
-  const array = req.body;
-  console.log(array);
+  //receives an array of numbers => [  1, 11, 12, 90, 3,  8,  2]
+  let inputArr = req.body;
+  let responseArr = [[...req.body]];
+
+  let sortAlgo = (inputArr) => {
+    let n = inputArr.length;
+    let stepArr;
+
+    let i, key, j;
+    for (i = 1; i < n; i++) {
+      key = inputArr[i];
+      j = i - 1;
+
+      /* Move elements of arr[0..i-1], that are 
+          greater than key, to one position ahead 
+          of their current position */
+      while (j >= 0 && inputArr[j] > key) {
+        inputArr[j + 1] = inputArr[j];
+        j = j - 1;
+      }
+      inputArr[j + 1] = key;
+      // print a copy the array after the iteration
+      stepArr = [...inputArr];
+      // save an snapshot of the array after the iteration
+      responseArr.push(stepArr);
+    }
+    return inputArr;
+  };
+
+  let outputArray = await sortAlgo(inputArr);
 
   try {
-    res.json({ respuesta: [1, 2, 3, 4, 5, 6] });
+    res.json({ response: responseArr });
   } catch (error) {
     res.send(error);
   }
