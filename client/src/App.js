2e7a5be82e1145bcb5cbf532a15ddaccda313b63
mariachi9999 <57735911+mariachi9999@users.noreply.github.com>
2021-11-29 16:22:03 -0300

    deleting some lines on app js which doesnt need

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,11 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import { useState } from "react";
-import axios from "axios";
 import { Route, Routes } from "react-router-dom";
 import About from "./components/About/About";
 import Homepage from "./components/Homepage/Homepage";
 
 function App() {
-  const [state, setState] = useState({
-    inputValues: "",
-    selectedSort: "",
-    outputValues: [],
-  });
-
-  const sortClick = async () => {
-    if (state.selectedSort === "") {
-      alert(
-        "Upss...it seems that you dont select an algorithm! Pick one, please!"
-      );
-    } else if (state.inputValues === "") {
-      alert(
-        "Upss...it seems that you dont add numbers on input checkbox! Do it, please!"
-      );
-    } else {
-      let route = state.selectedSort.toLowerCase();
-      let split = state.inputValues.split(",");
-      let body = split.map((stringNumber) => Number(stringNumber));
-      let response = await axios.post(`http://localhost:3001/${route}`, body);
-      setState((prevState) => {
-        return {
-          ...prevState,
-          outputValues: response.data.response,
-        };
-      });
-    }
-  };
-
-  const clearClick = () => {
-    setState({
-      inputValues: [],
-      selectedSort: "",
-      outputValues: [],
-    });
-  };
-
   return (
     <div className="App">
       <Header />
