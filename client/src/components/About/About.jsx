7b92a5b4e63c0d786aa613c7bd202fe480bda0ec
mariachi9999 <57735911+mariachi9999@users.noreply.github.com>
2021-11-29 12:46:24 -0300

    installing react router dom in client folder

---
+++ client/src/components/About/About.jsx
@@ -0,0 +1,15 @@
+import "./About.module.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
+function About() {
+  return (
+    <div className="About">
+      <Header />
+      <div class="container"></div>
+      <Footer />
+    </div>
+  );
+}
+
+export default About;
