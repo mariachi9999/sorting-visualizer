ed2a394420fa11fa67f1554d22aa36e33484e101
mariachi9999 <57735911+mariachi9999@users.noreply.github.com>
2021-11-29 02:16:25 -0300

    working on graph

--- client/src/components/GraphSort/GraphSort.jsx
+++ client/src/components/GraphSort/GraphSort.jsx
@@ -2,20 +2,60 @@ import React from "react";
 import styles from "./GraphSort.module.css";
 import { Bar } from "react-chartjs-2";
 import Chart from "chart.js/auto";
-const state = {
-  labels: ["January", "February", "March", "April", "May"],
-  datasets: [
-    {
-      label: "Rainfall",
-      backgroundColor: "rgba(75,192,192,1)",
-      borderColor: "rgba(0,0,0,1)",
-      borderWidth: 2,
-      data: [65, 59, 80, 81, 56],
-    },
-  ],
-};
+import { useState, useEffect } from "react";
 
-function GraphSort({ chartData }) {
+// const state = {
+//   labels: ["January", "February", "March", "April", "May"],
+//   datasets: [
+//     {
+//       label: "Rainfall",
+//       backgroundColor: "rgba(75,192,192,1)",
+//       borderColor: "rgba(0,0,0,1)",
+//       borderWidth: 2,
+//       data: [65, 59, 80, 81, 56],
+//     },
+//   ],
+// };
+
+function GraphSort({ outputValues }) {
+  let title = outputValues?.algoInfo?.title || "Sort";
+  let operations = outputValues?.sortedArr || [];
+  console.log(operations);
+
+  useEffect(() => {
+    operations.forEach((operation) => {
+      let labels = [];
+      for (let i = 0; i < operation.length; i++) {
+        labels.push(i + 1);
+      }
+      setState({
+        labels: labels,
+        datasets: [
+          {
+            label: title,
+            backgroundColor: "rgba(75,192,192,1)",
+            borderColor: "rgba(0,0,0,1)",
+            borderWidth: 2,
+            data: operation,
+          },
+        ],
+      });
+    });
+  }, [operations]);
+
+  const [state, setState] = useState({
+    labels: ["January", "February", "March", "April", "May"],
+    datasets: [
+      {
+        label: "Rainfall",
+        backgroundColor: "rgba(75,192,192,1)",
+        borderColor: "rgba(0,0,0,1)",
+        borderWidth: 2,
+        data: [65, 59, 80, 81, 56],
+      },
+    ],
+  });
+
   return (
     <div className={styles.graphContainer}>
       <Bar
