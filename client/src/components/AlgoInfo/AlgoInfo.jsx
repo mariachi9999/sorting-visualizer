e218de7302392a4b09c0059fe8a5ba4b3fd213c8
mariachi9999 <57735911+mariachi9999@users.noreply.github.com>
2021-11-29 01:37:05 -0300

    AlgoInfo receiving values from state and rendering ok

--- client/src/components/AlgoInfo/AlgoInfo.jsx
+++ client/src/components/AlgoInfo/AlgoInfo.jsx
@@ -1,48 +1,51 @@
 import React from "react";
 import styles from "./AlgoInfo.module.css";
 
-function AlgoInfo() {
+function algoInfo({ outputValues }) {
+  let { algoInfo, sortedArr } = outputValues;
+  console.log(outputValues);
   return (
     <div id={styles.container} className={styles.text}>
-      <h3 className={styles.title}>Algorithm Title</h3>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto commodi
-        dolorum, optio esse excepturi dicta aspernatur dolor molestiae sequi
-        ducimus eaque error, corrupti quos! Ullam molestias accusamus
-        praesentium nihil assumenda.
-      </p>
-      <div className="container">
-        <div className="container-box1">
-          <div>
-            <span>Best: </span>
-            <p>Value</p>
-          </div>
-          <div>
-            <span>Average: </span>
-            <p>Value</p>
-          </div>
-          <div>
-            <span>Worst: </span>
-            <p>Value</p>
+      {algoInfo ? (
+        <div>
+          <h3 className={styles.title}>{algoInfo.title}</h3>
+          <p>{algoInfo.description}</p>
+          <div className="container">
+            <div className="container-box1">
+              <div>
+                <span>Best: </span>
+                <p>{algoInfo.best}</p>
+              </div>
+              <div>
+                <span>Average: </span>
+                <p>{algoInfo.average}</p>
+              </div>
+              <div>
+                <span>Worst: </span>
+                <p>{algoInfo.worst}</p>
+              </div>
+            </div>
+            <div className="container-box1">
+              <div>
+                <span>Memory: </span>
+                <p>{algoInfo.memory}</p>
+              </div>
+              <div>
+                <span>Stability: </span>
+                <p>{algoInfo.stable}</p>
+              </div>
+              <div>
+                <span>Method: </span>
+                <p>{algoInfo.method}</p>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="container-box1">
-          <div>
-            <span>Memory: </span>
-            <p>Value</p>
-          </div>
-          <div>
-            <span>Stability: </span>
-            <p>Value</p>
-          </div>
-          <div>
-            <span>Method: </span>
-            <p>Value</p>
-          </div>
-        </div>
-      </div>
+      ) : (
+        <div id={styles.empty}></div>
+      )}
     </div>
   );
 }
 
-export default AlgoInfo;
+export default algoInfo;
