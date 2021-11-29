c8e74a795daa2c73ccac6e9bd8e8968be5b7af13
mariachi9999 <57735911+mariachi9999@users.noreply.github.com>
2021-11-28 21:39:45 -0300

    InputValues ok, wo css

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header/Header";
+import InputValues from "./components/InputValues/InputValues";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <InputValues />
     </div>
   );
 }
---
+++ client/src/components/InputValues/InputValues.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./InputValues.module.css";
+import CalculateIcon from "@mui/icons-material/Calculate";
+
+function InputValues() {
+  return (
+    <div id={styles.container}>
+      <textarea></textarea>
+    </div>
+  );
+}
+
+export default InputValues;
