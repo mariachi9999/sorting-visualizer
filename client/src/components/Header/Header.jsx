2c76f59438a0fd65e0af601382933bcb6316728c
ChainFoundation <Jorge.Chain@jala-foundation.org>
2021-11-29 00:29:54 -0400

    fixed logo

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
   return (
     <div id={styles.container} className="header" >
       <span>
-        <CalculateIcon className="icon"/>
+        <CalculateIcon className={styles.icon} sx={{ fontSize: 60 }} />
       </span>
       <h1 className={styles.name}>Algorithm Visualizer</h1>
       <h2 className={styles.about}>About Us</h2>
