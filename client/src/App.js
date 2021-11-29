adec4bfd67cf78db6368694b09223a1a47e2344a
mariachi9999 <57735911+mariachi9999@users.noreply.github.com>
2021-11-28 21:58:05 -0300

    SelectSort wireframe ok

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import InputValues from "./components/InputValues/InputValues";
 import AlgoInfo from "./components/AlgoInfo/AlgoInfo";
+import SelectSort from "./components/SelectSort/SelectSort";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <InputValues />
       <AlgoInfo />
+      <SelectSort />
     </div>
   );
 }
---
+++ client/src/components/SelectSort/SelectSort.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import styles from "./SelectSort.module.css";
+
+function SelectSort() {
+  return (
+    <div id={styles.container}>
+      <form id={styles.form}>
+        <label>
+          Bubble Sort
+          <input
+            name="Bubble"
+            type="checkbox"
+            // checked={}
+            // onChange={}
+          />
+        </label>
+        <label>
+          Heap Sort
+          <input
+            name="Heap"
+            type="checkbox"
+            // checked={}
+            // onChange={}
+          />
+        </label>
+        <label>
+          Insertion Sort
+          <input
+            name="Insertion"
+            type="checkbox"
+            // checked={}
+            // onChange={}
+          />
+        </label>
+        <label>
+          Merge Sort
+          <input
+            name="Merge"
+            type="checkbox"
+            // checked={}
+            // onChange={}
+          />
+        </label>
+        <label>
+          Quick Sort
+          <input
+            name="Quick"
+            type="checkbox"
+            // checked={}
+            // onChange={}
+          />
+        </label>
+        <label>
+          Selection Sort
+          <input
+            name="Selection"
+            type="checkbox"
+            // checked={}
+            // onChange={}
+          />
+        </label>
+      </form>
+    </div>
+  );
+}
+
+export default SelectSort;
