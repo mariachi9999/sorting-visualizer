078b4fe54ffab0209915ecbffd26e689acc91cdb
mariachi9999 <57735911+mariachi9999@users.noreply.github.com>
2021-11-29 01:52:13 -0300

    OutputValues render the sorted array

--- client/src/components/OutputValues/OutputValues.jsx
+++ client/src/components/OutputValues/OutputValues.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import styles from "./OutputValues.module.css";
+import { useState, useEffect } from "react";
 
-function OutputValues() {
+function OutputValues({ outputValues }) {
+  const [state, setState] = useState("");
+
+  useEffect(() => {
+    outputValues?.sortedArr?.at(-1)
+      ? setState(outputValues.sortedArr.at(-1))
+      : setState("");
+  }, [outputValues]);
+
   return (
-    <div id={styles.container} >
-      <textarea className={styles.outArea}></textarea>
+    <div id={styles.container}>
+      <textarea className={styles.outArea} value={state}></textarea>
     </div>
   );
 }
