374e922cfbe29bf5757287fa3b0d55b80c5659e2
mariachi9999 <57735911+mariachi9999@users.noreply.github.com>
2021-11-29 01:39:22 -0300

    adding title to database sorting

--- api/database/database.js
+++ api/database/database.js
@@ -1,5 +1,6 @@
 const database = {
   bubble_sort: {
+    title: "Bubble Sort",
     description:
       "This sorting algorithm is comparison-based algorithm in which each pair of adjacent elements is compared and the elements are swapped if they are not in order. This algorithm is not suitable for large data sets.",
     best: "Ο(n)",
@@ -10,6 +11,7 @@ const database = {
     method: "Exchanging",
   },
   heap_sort: {
+    title: "Heap Sort",
     description:
       "The Heapsort algorithm involves preparing the list by first turning it into a max heap. The algorithm then repeatedly swaps the first value of the list with the last value, decreasing the range of values considered in the heap operation by one, and sifting the new first value into its position in the heap.",
     best: "Ο(nlogn)",
@@ -20,6 +22,7 @@ const database = {
     method: "Selection",
   },
   insertion_sort: {
+    title: "Insertion Sort",
     description:
       "Insertion sort is the sorting mechanism where the sorted array is built having one item at a time. The array elements are compared with each other sequentially and then arranged simultaneously in some particular order. The analogy can be understood from the style we arrange a deck of cards.",
     best: "Ο(n)",
@@ -30,6 +33,7 @@ const database = {
     method: "Insertion",
   },
   merge_sort: {
+    title: "Merge Sort",
     description:
       "Merge sort is one of the most efficient sorting algorithms. It works on the principle of Divide and Conquer. Merge sort repeatedly breaks down a list into several sublists until each sublist consists of a single element and merging those sublists in a manner that results into a sorted list.",
     best: "Ο(nlogn)",
@@ -40,6 +44,7 @@ const database = {
     method: "Merging",
   },
   quick_sort: {
+    title: "Quick Sort",
     description:
       "It is one of the most efficient sorting algorithms and is based on the splitting of an array into smaller ones and swapping based on the comparison with 'pivot' element selected. Due to this, quick sort is also called as \"Partition Exchange\" sort. It falls into the category of divide and conquer approach of problem-solving methodology.",
     best: "Ο(nlogn)",
@@ -50,6 +55,7 @@ const database = {
     method: "Partitioning",
   },
   selection_sort: {
+    title: "Selection Sort",
     description:
       "This sorting algorithm is comparison-based algorithm in which each pair of adjacent elements is compared and the elements are swapped if they are not in order. This algorithm is not suitable for large data sets.",
     best: "Ο(n^2)",
